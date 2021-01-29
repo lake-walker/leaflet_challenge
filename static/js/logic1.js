b013ceb6fc7b12ed4cd47ce4c5d2d680eef75cb1
Lake Brant <lake.brant@du.edu>
2021-01-28 18:37:44 -0700

    changed to 7 day total instead of 30 day total

--- static/js/logic1.js
+++ static/js/logic1.js
@@ -8,7 +8,7 @@ var myMap = L.map('map', {
 // var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
 //   "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
-var queryUrl =  'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson';
+var queryUrl =  'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
